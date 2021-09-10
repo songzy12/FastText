809630aa81e26a4e5e40f71bb4874a0670952139
Zhengyang Song <songzy_thu@163.com>
2021-09-10 22:59:49 +0800

    Add YahooAnswers dataset. #2.

---
+++ yahoo_answers.py
@@ -0,0 +1,41 @@
+import os
+import collections
+
+import pandas as pd
+from paddlenlp.utils.env import DATA_HOME
+from paddlenlp.datasets import DatasetBuilder
+
+__all__ = ['YahooAnswers']
+
+
+class YahooAnswers(DatasetBuilder):
+    """
+    The data is from https://arxiv.org/pdf/1509.01626.pdf.
+    """
+    META_INFO = collections.namedtuple('META_INFO', ('file', 'md5'))
+    SPLITS = {
+        'train': META_INFO(
+            os.path.join('yahoo_answers_csv', 'train.csv'), None),
+        'test':
+        META_INFO(os.path.join('yahoo_answers_csv', 'test.csv'), None)
+    }
+
+    def _get_data(self, mode, **kwargs):
+        default_root = os.path.join(DATA_HOME, self.__class__.__name__)
+        filename, _ = self.SPLITS[mode]
+        fullname = os.path.join(default_root, filename)
+
+        return fullname
+
+    def _read(self, filename, *args):
+        data = pd.read_csv(filename)
+        for class_index, question_title, question_content, best_answer in data.values:
+            text = question_title
+            if type(question_content) is str:
+                text += '\n' + question_content
+            if type(best_answer) is str:
+                text += '\n' + best_answer
+            yield {"text": text, "label": class_index}
+
+    def get_labels(self):
+        return range(1, 11)
